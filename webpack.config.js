cb0b197c7634f9b641a7b355cef913735edc9f00
Katia Toboleva <katia@Katias-MacBook-Air.local>
2021-08-26 15:51:51 +0100

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+
+const webpackTemplate = require('./client/webpack/template');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = ({ NODE_ENV }) => ({
+  mode: NODE_ENV,
+  entry: './client/app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                indentWidth: 2,
+                outputStyle: 'expanded',
+                includePaths: [
+                  path.resolve(__dirname, 'client/app/styles'),
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve('./client/app/components'),
+      '@containers': path.resolve('./client/app/containers'),
+      '@routes': path.resolve('./client/app/routes'),
+    },
+  },
+  devServer: {
+    publicPath: '/',
+    historyApiFallback: true,
+    open: true,
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost',
+        target: 'http://localhost:3000',
+        onProxyReq: (proxyReq) => {
+          if (proxyReq.getHeader('origin')) {
+            proxyReq.setHeader('origin', 'http://localhost:3000');
+          }
+        }
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: webpackTemplate,
+    }),
+  ]
+});
