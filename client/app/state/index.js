20b0ef985307f65c9730b95a541c8b1bde72e4a8
Katia Toboleva <katia@Katias-MacBook-Air.local>
2021-08-26 16:29:44 +0100

    redux store setup

---
+++ client/app/state/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import rootReducer from './reducers';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export {
+  rootReducer,
+  store,
+};
+
---
+++ client/app/state/utilities.js
@@ -0,0 +1,9 @@
+export const createReducer = ({ initialState = {}, atoms = {} }) => (
+  state = initialState, action = {},
+) => {
+  if (typeof atoms[action.type] === 'function') {
+    return { ...state, ...atoms[action.type](state, action.payload || {}) || {} };
+  }
+
+  return state;
+};
