0dc25c1161d749e451621e33be3f3c476a20c25c
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-08-30 19:06:27 +0100

    modal

--- client/app/components/modal/panels/view-item/quantitySelector/quantitySelector.jsx
+++ client/app/components/modal/panels/view-item/quantitySelector/quantitySelector.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Text from '../../../../text';
+import Button from '../../../../button';
 import styles from './styles.scss';
 
 const QuantitySelector = ({
   quantity,
   onQuantityChange,
+  onMinus,
+  onPlus,
 }) => {
   return (
     <div className={styles.container}>
       <Text text="Quantity" weight="bold"/>
-      <input onChange={onQuantityChange} value={quantity}/>
+      <div className={styles.buttons}>
+        <Button onClick={onMinus}>
+          <Text text="-"/>
+        </Button>
+        <input onChange={onQuantityChange} value={quantity}/>
+        <Button onClick={onPlus}>
+          <Text text="+"/>
+        </Button>
+      </div>
     </div>
   );
 };
