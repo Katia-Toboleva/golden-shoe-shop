5a263e59f2f3c9379af106e4be48dff39484e8b1
Katia Toboleva <katia@Katias-MacBook-Air.local>
2021-09-01 01:49:53 +0100

    updated helper

--- client/app/utilities/helpers.js
+++ client/app/utilities/helpers.js
@@ -1,6 +1,13 @@
 export const findImage = (color, images) => images.find(el => el.color === color).urls[0];
 
-export const calculateSubtotal = (arr) => arr.reduce((acc, currentVal) => acc + (currentVal.price * currentVal.selectedOptions.quantity), 0);
+export const calculateSubtotal = (arr) => arr.reduce((acc, currentVal) => {
+  // If item is not available, its price is not added to the total
+  if (!currentVal.isItemAvailable) {
+    return acc;
+  }
+
+  return acc + (currentVal.price * currentVal.selectedOptions.quantity);
+}, 0);
 
 export const calculateTotal = (arr, tax) => {
   const subtotal = calculateSubtotal(arr);
