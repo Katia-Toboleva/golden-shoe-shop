9c0d1b0b2d834afd21897b04863526c2b7cdb9c1
Katia Toboleva <katia@Katias-MacBook-Air.local>
2021-09-02 21:44:09 +0100

    favicon

--- client/webpack/template.js
+++ client/webpack/template.js
@@ -6,7 +6,9 @@ module.exports = ({ htmlWebpackPlugin }) => {
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>APP</title>
+          <link rel="icon" type="image/png" href="client/app/components/logo/golden_shoe_logo.png"/>
+          
+          <title>Golden Shoe</title>
         </head>
         <body>
           <div id="app"></div>
